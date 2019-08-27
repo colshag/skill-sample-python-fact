a2b41bbd6f135ee092a3406c7f34916e7028917f
Chris Lewis <colshag@gmail.com>
2019-08-27 15:48:32 -0600

    initial data entry for skill'

--- lambda/py/lambda_function.py
+++ lambda/py/lambda_function.py
@@ -16,9 +16,9 @@ from ask_sdk_model import Response
 
 
 # =========================================================================================================================================
-# TODO: The items below this comment need your attention.
+# GLOBALS
 # =========================================================================================================================================
-SKILL_NAME = "Space Facts"
+SKILL_NAME = "Cosmica"
 GET_FACT_MESSAGE = "Here's your fact: "
 HELP_MESSAGE = "You can say tell me a space fact, or, you can say exit... What can I help you with?"
 HELP_REPROMPT = "What can I help you with?"
@@ -27,9 +27,98 @@ FALLBACK_MESSAGE = "The Space Facts skill can't help you with that.  It can help
 FALLBACK_REPROMPT = 'What can I help you with?'
 EXCEPTION_MESSAGE = "Sorry. I cannot help you with that."
 
+SCENARIO_NAME = 0
+DIALOG = 1
+CHOICE1 = 2
+CHOICE2 = 3
+EXIT = 4
+CREW_MORALE = 5
+SHIP_STRENGTH = 6
+INTEL = 7
+
 # =========================================================================================================================================
-# TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-python-fact/lambda/data
+# Scenario Data
 # =========================================================================================================================================
+
+scenarios = [] # scenario data
+scenarios_seen = [] # keep track of scenarios that have been seen so far
+
+
+scenarios.append({SCENARIO_NAME:'put name of scenario here',
+                  DIALOG:'put dialog here',
+                  CHOICE1:'choice should be YES-1 or NO-2, etc where the 1 and 2 are the array ID of next scenario to go to',
+                  CHOICE2:'other possible choice',
+                  EXIT:'type EXIT here if this dialog then ends the scenario starting a new one',
+                  CREW_MORALE:'place a positive or negative integer if crew morale is affected by this outcome',
+                  SHIP_STRENGTH:'place a positive or negative integer if ship strength is affected by this outcome',
+                  INTEL:'place a positive or negative integer if intel is affected by this outcome'})
+
+# temp load of scenario for testing
+scenarios.append({SCENARIO_NAME:'DISTRESS CALL 1',
+                  DIALOG:'Captain, I have picked up a distress call coming from the vega system, would you like for us to respond?',
+                  CHOICE1:'YES-3',
+                  CHOICE2:'NO-2',
+                  EXIT:'',
+                  CREW_MORALE:'',
+                  SHIP_STRENGTH:'',
+                  INTEL:''})
+scenarios.append({SCENARIO_NAME:'DISTRESS CALL 1',
+                  DIALOG:'Aye Aye Captain, we will avoid this beacon. Spock Says, Good thinking Captain, after further analysis, I believe this signal shows a similar pattern to onse that the USS Cole fell victom to as an Alrick Von Monicko decoy. We might have gained some interesting intel though.',
+                  CHOICE1:'',
+                  CHOICE2:'',
+                  EXIT:'EXIT',
+                  CREW_MORALE:'',
+                  SHIP_STRENGTH:'',
+                  INTEL:'5'})
+scenarios.append({SCENARIO_NAME:'DISTRESS CALL 1',
+                  DIALOG:'Captain, we are arriving at the coordinates of the distress call. Ohura calls out, Captain we are reading multiple distress calls from a vulcan cruiser, Spock Says, Captain, scans show multiple life forms on the ship, however their signals are weaker than expected. Shall we send a boarding crew to investigate?',
+                  CHOICE1:'YES-4',
+                  CHOICE2:'NO-2',
+                  EXIT:'',
+                  CREW_MORALE:'',
+                  SHIP_STRENGTH:'',
+                  INTEL:''})
+scenarios.append({SCENARIO_NAME:'DISTRESS CALL 1',
+                  DIALOG:'You beam on board the Cruiser and notice a strange acidic smell that burns your nostrils. After placing breathing masks Seargent Oleg calls out, Captain! Over here! I am detecting multiple strange life signals from the room ahead. Scotty also calls out, Captain! I am detecting a strange power surge from what looks like the engine room, this could be trouble, the hyper drive could be going critical soon. Say Engine Room or Life forms to direct your crews next action.',
+                  CHOICE1:'ENGINE ROOM-5',
+                  CHOICE2:'LIFE FORMS-6',
+                  EXIT:'',
+                  CREW_MORALE:'',
+                  SHIP_STRENGTH:'',
+                  INTEL:''})
+scenarios.append({SCENARIO_NAME:'DISTRESS CALL 1',
+                  DIALOG:'Scotty says, Captain, it looks like the hyper drive is in critical condition and unstable, at most we have minutes before the drive blows. If I focus on this, we will not have enough time to deal with the survivors, do you want us to stay or resecue the survivors? Say Stay or Rescue to direct your crews next action.',
+                  CHOICE1:'STAY-8',
+                  CHOICE2:'RESCUE-6',
+                  EXIT:'',
+                  CREW_MORALE:'',
+                  SHIP_STRENGTH:'',
+                  INTEL:''})
+scenarios.append({SCENARIO_NAME:'DISTRESS CALL 1',
+                  DIALOG:'You manage to access the room and your crew discoveres a handful of vulcan aliens tending to their wounded.  Scotty calls out, Captain! I am detecting a dangerous increase in hyperdrive levels from the engine room, we need to beam off this ship! Do you bring the wounded vulcan aliens with your crew?',
+                  CHOICE1:'YES-7',
+                  CHOICE2:'NO-8',
+                  EXIT:'',
+                  CREW_MORALE:'',
+                  SHIP_STRENGTH:'',
+                  INTEL:''})
+scenarios.append({SCENARIO_NAME:'DISTRESS CALL 1',
+                  DIALOG:'The Cruiser explodes at a safe distance and after some time in the medical bay Ohura declares, Captain, we managed to piece together what happened, it is a good thing we saved this crew, we learned some valueable information about Von Monikos pirate fleet from some of the discussions and readings they brought with them as we were leaving the ship.',
+                  CHOICE1:'',
+                  CHOICE2:'',
+                  EXIT:'EXIT',
+                  CREW_MORALE:'',
+                  SHIP_STRENGTH:'',
+                  INTEL:'10'})
+scenarios.append({SCENARIO_NAME:'DISTRESS CALL 1',
+                  DIALOG:'The Cruiser explodes at a safe distance and after some time the Scotty says, Captain, we managed to gather some interesting technology which we think will help increase our ships strength, however, the crew is not happy that we abandoned a crew in need.',
+                  CHOICE1:'',
+                  CHOICE2:'',
+                  EXIT:'EXIT',
+                  CREW_MORALE:'-10',
+                  SHIP_STRENGTH:'10',
+                  INTEL:''})
+
 
 data = [
   'A year on Mercury is just 88 days long.',
@@ -189,8 +278,8 @@ sb.add_request_handler(SessionEndedRequestHandler())
 sb.add_exception_handler(CatchAllExceptionHandler())
 
 # TODO: Uncomment the following lines of code for request, response logs.
-# sb.add_global_request_interceptor(RequestLogger())
-# sb.add_global_response_interceptor(ResponseLogger())
+sb.add_global_request_interceptor(RequestLogger())
+sb.add_global_response_interceptor(ResponseLogger())
 
 # Handler name that is used on AWS lambda
 lambda_handler = sb.lambda_handler()
